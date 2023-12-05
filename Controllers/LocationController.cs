7279973bb550442be5a6979ce2d3d54a1f2ed2e4
Thế Anh Đặng <theanh99huha@gmail.com>
2023-12-05 20:01:05 +0700

    Add location API

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetLocation()
+        {
+            var locations = new string[]
+            {
+                "Locate 1", "Locate 2"
+            };
+
+            return Ok(locations);
+        }
+    }
+}
